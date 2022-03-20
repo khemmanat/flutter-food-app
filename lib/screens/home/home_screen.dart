860448ed3ffb93f94a7e58057f468b8aca9c52e0
Kim Khemmanat <khemmanat2012@gmail.com>
2022-03-20 16:41:52 +0700

    complete basic homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterfoodappbase/constants/constants.dart';
 import 'package:flutterfoodappbase/mock/mockData.dart';
 import 'package:flutterfoodappbase/screens/home/components/components.dart';
+import 'package:flutterfoodappbase/screens/home/components/restaurant_info_medium_card.dart';
 import 'package:flutterfoodappbase/screens/home/components/section_title.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,8 +15,9 @@ class HomeScreen extends StatelessWidget {
         body: CustomScrollView(
       slivers: [
         SliverAppBar(
-          backgroundColor: Colors.transparent,
+          backgroundColor: Colors.white,
           elevation: 0,
+          floating: true,
           centerTitle: true,
           title: Column(
             children: [
@@ -48,48 +50,60 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         SliverToBoxAdapter(
-            child: Row(
-          children: [
-            SizedBox(
-                width: 200,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AspectRatio(
-                        aspectRatio: 1.25,
-                        child: Image.asset("assets/images/medium_1.png")),
-                    Text(demoMediumCardData[0]['name'],
-                        style: Theme.of(context).textTheme.headline6),
-                    Text(demoMediumCardData[0]["location"],
-                        style: const TextStyle(color: kBodyTextColor)),
-                    DefaultTextStyle(
-                      style: const TextStyle(color: Colors.black, fontSize: 12),
-                      child: Row(
-                        children: [
-                          Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: defaultPadding / 2,
-                                  vertical: defaultPadding / 8),
-                              decoration: const BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(6)),
-                                  color: kActiveColor),
-                              child: const Text("4.6",
-                                  style: TextStyle(color: Colors.white))),
-                          Spacer(),
-                          Text("25 mins"),
-                          Spacer(),
-                          CircleAvatar(
-                              radius: 2, backgroundColor: Color(0xFF868686)),
-                          Spacer(),
-                          Text("Free Delivery")
-                        ],
-                      ),
-                    )
-                  ],
-                )),
-          ],
-        ))
+            child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              demoMediumCardData.length,
+              (index) => Padding(
+                padding: const EdgeInsets.only(left: defaultPadding),
+                child: RestaurantInfoMediumCard(
+                  image: demoMediumCardData[index]["image"],
+                  title: demoMediumCardData[index]["name"],
+                  location: demoMediumCardData[index]["location"],
+                  deliveryTime: demoMediumCardData[index]["deliveryTime"],
+                  rating: demoMediumCardData[index]["rating"],
+                  press: () {},
+                ),
+              ),
+            ),
+          ),
+        )),
+        SliverPadding(
+          padding: const EdgeInsets.all(defaultPadding),
+          sliver: SliverToBoxAdapter(
+            child: Image.asset("assets/images/Banner.png"),
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.all(defaultPadding),
+          sliver: SliverToBoxAdapter(
+            child: SectionTitle(
+              title: "Best Picks",
+              press: () {},
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+            child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              demoMediumCardData.length,
+              (index) => Padding(
+                padding: const EdgeInsets.only(left: defaultPadding),
+                child: RestaurantInfoMediumCard(
+                  image: demoMediumCardData[index]["image"],
+                  title: demoMediumCardData[index]["name"],
+                  location: demoMediumCardData[index]["location"],
+                  deliveryTime: demoMediumCardData[index]["deliveryTime"],
+                  rating: demoMediumCardData[index]["rating"],
+                  press: () {},
+                ),
+              ),
+            ),
+          ),
+        )),
       ],
     ));
   }
